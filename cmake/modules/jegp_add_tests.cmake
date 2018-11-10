cd00348a7d9581af78d0e5b696bc31c695af72ed
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2018-11-09 22:56:12 -0400

    Only build the tests

--- cmake/modules/jegp_add_tests.cmake
+++ cmake/modules/jegp_add_tests.cmake
@@ -2,7 +2,7 @@ function(jegp_add_test name)
     add_executable(jegp_test_${name} test/${name}.cpp)
     target_compile_options(jegp_test_${name} PRIVATE ${ARGN})
     target_link_libraries(jegp_test_${name} PRIVATE jegp::jegp)
-    add_test(jegp_test_${name} jegp_test_${name})
+#    add_test(jegp_test_${name} jegp_test_${name})
 endfunction()
 
 jegp_add_test(include_utility)
