2b3e5de89c61e72f63bbbc5c33e484abe9e25854
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2019-06-26 21:09:30 -0400

    Revert "Remove travis/install_cmake.sh [skip ci]"
    This reverts commit 36700a66c9038e8c17e033610bb3f7edabeebf1e.

---
+++ travis/install_cmake.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ev
+
+cmake_version=3.12
+cmake_patch=0
+cmake_pre=
+cmake_installer=cmake-${cmake_version}.${cmake_patch}${cmake_pre}-Linux-x86_64.sh
+
+cd /tmp
+wget https://cmake.org/files/v${cmake_version}/${cmake_installer}
+sh ${cmake_installer} --prefix=. --exclude-subdir
