5946c38c48a56107eaaeaa8e75468e71ae57faa0
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2019-06-27 13:53:40 -0400

    Fix link

--- travis/install_cmake.sh
+++ travis/install_cmake.sh
@@ -7,5 +7,5 @@ cmake_version=$1
 cmake_installer=cmake-${cmake_version}-Linux-x86_64.sh
 
 cd /tmp
-wget https://cmake.org/files/v${cmake_version}/${cmake_installer}
+wget https://github.com/Kitware/CMake/releases/download/v${cmake_version}/${cmake_installer}
 sh ${cmake_installer} --prefix=. --exclude-subdir
