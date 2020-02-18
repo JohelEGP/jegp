05dae5692be0bd51b64a86fd4586cafc8729b65f
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2020-02-18 19:30:24 -0400

    Add missing alias for installed builds

--- cmake/jegp-config.cmake
+++ cmake/jegp-config.cmake
@@ -3,3 +3,6 @@ include(CMakeFindDependencyMacro)
 find_dependency(range-v3 0.10.0)
 
 include(${CMAKE_CURRENT_LIST_DIR}/jegp-targets.cmake)
+
+add_library(jegp::jegp INTERFACE IMPORTED)
+target_link_libraries(jegp::jegp INTERFACE jegp)
