c3e8e6a0c7a6f0b993e382fd9b1fa45fb77407dd
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2022-09-15 15:40:06 -0400

    chore: enable GCC

--- cmake/JEGPFindDependencies.cmake
+++ cmake/JEGPFindDependencies.cmake
@@ -2,11 +2,11 @@ include(FetchContent)
 FetchContent_Declare(
     CPM
     GIT_REPOSITORY https://github.com/TheLartians/CPM.cmake
-    GIT_TAG master
+    GIT_TAG v0.35.6
     GIT_SHALLOW True)
 FetchContent_MakeAvailable(CPM)
 include(${cpm_SOURCE_DIR}/cmake/CPM.cmake)
 
 CPMAddPackage("gh:johelegp/jegp.cmake_modules#master")
 
-list(APPEND CMAKE_MODULE_PATH "${jegp.cmake_modules_SOURCE_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${jegp.cmake_modules_SOURCE_DIR}/modules")
